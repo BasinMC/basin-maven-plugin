48556526434e209ebc33ff2ff887939e446f3a5c
Johannes Donath <johannesd@torchmind.com>
2018-03-02 15:47:56 +0100

    Added support for reading libraries.

--- src/main/java/org/basinmc/maven/basin/launcher/GameVersion.java
+++ src/main/java/org/basinmc/maven/basin/launcher/GameVersion.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -33,6 +34,11 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
 
@@ -43,7 +49,6 @@ import org.apache.commons.codec.binary.Hex;
  */
 @JsonIgnoreProperties({
     "assetIndex",
-    "libraries",
     "logging"
 })
 public final class GameVersion {
@@ -51,32 +56,40 @@ public final class GameVersion {
   private final String assets;
   private final Downloads downloads;
   private final String id;
+  private final Set<Library> libraries;
+
   private final String mainClass;
   private final String minecraftArguments;
   private final int minimumLauncherVersion;
+
   private final Instant releaseTime;
   private final Instant time;
   private final VersionType type;
 
-  public GameVersion(
+  @JsonCreator
+  private GameVersion(
       @JsonProperty(value = "minimumLauncherVersion", required = true) int minimumLauncherVersion,
       @NonNull @JsonProperty(value = "id", required = true) String id,
       @NonNull @JsonProperty(value = "assets", required = true) String assets,
+      @NonNull @JsonProperty(value = "downloads", required = true) Downloads downloads,
+      @NonNull @JsonProperty(value = "libraries", required = true) Set<Library> libraries,
       @NonNull @JsonProperty(value = "mainClass", required = true) String mainClass,
       @NonNull @JsonProperty(value = "minecraftArguments", required = true) String minecraftArguments,
       @NonNull @JsonProperty(value = "releaseTime", required = true) Instant releaseTime,
       @NonNull @JsonProperty(value = "time", required = true) Instant time,
-      @NonNull @JsonProperty(value = "type", required = true) String type,
-      @NonNull @JsonProperty(value = "downloads", required = true) Downloads downloads) {
+      @NonNull @JsonProperty(value = "type", required = true) String type) {
     this.minimumLauncherVersion = minimumLauncherVersion;
+
     this.id = id;
     this.assets = assets;
+    this.downloads = downloads;
+    this.libraries = libraries;
+
     this.mainClass = mainClass;
     this.minecraftArguments = minecraftArguments;
     this.releaseTime = releaseTime;
     this.time = time;
     this.type = VersionType.valueOf(type.toUpperCase());
-    this.downloads = downloads;
   }
 
   public String getAssets() {
@@ -89,6 +102,11 @@ public final class GameVersion {
 
   public String getId() {
     return this.id;
+  }
+
+  @NonNull
+  public Set<Library> getLibraries() {
+    return Collections.unmodifiableSet(this.libraries);
   }
 
   public String getMainClass() {
@@ -200,7 +218,7 @@ public final class GameVersion {
     private final Download server;
 
     @JsonCreator
-    public Downloads(
+    private Downloads(
         @NonNull @JsonProperty(value = "client", required = true) Download client,
         @NonNull @JsonProperty(value = "server", required = true) Download server) {
       this.client = client;
@@ -215,6 +233,184 @@ public final class GameVersion {
     @NonNull
     public Download getServer() {
       return this.server;
+    }
+  }
+
+  /**
+   * (Partially) Represents a library on which this game version relies (including client
+   * libraries).
+   */
+  @JsonIgnoreProperties({
+      "extract",
+      "rules",
+      "natives"
+  })
+  public static final class Library {
+
+    private final String name;
+    private final LibraryDownloads downloads;
+
+    @JsonCreator
+    private Library(
+        @NonNull @JsonProperty(value = "name", required = true) String name,
+        @NonNull @JsonProperty(value = "downloads", required = true) LibraryDownloads downloads) {
+      this.name = name;
+      this.downloads = downloads;
+    }
+
+    @NonNull
+    public String getName() {
+      return this.name;
+    }
+
+    @NonNull
+    public LibraryDownloads getDownloads() {
+      return this.downloads;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || this.getClass() != o.getClass()) {
+        return false;
+      }
+      Library library = (Library) o;
+      return Objects.equals(this.name, library.name) &&
+          Objects.equals(this.downloads, library.downloads);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+      return Objects.hash(this.name, this.downloads);
+    }
+  }
+
+  public static final class LibraryDownloads {
+
+    private final Map<String, Artifact> classifiers;
+    private final Artifact artifact;
+
+    @JsonCreator
+    private LibraryDownloads(
+        @Nullable @JsonProperty("classifiers") Map<String, Artifact> classifiers,
+        @Nullable @JsonProperty("artifact") Artifact artifact) {
+      this.classifiers = classifiers != null ? classifiers : Collections.emptyMap();
+      this.artifact = artifact;
+    }
+
+    @NonNull
+    public Optional<Artifact> getClassifier(@NonNull String classifier) {
+      return Optional.ofNullable(this.classifiers.get(classifier));
+    }
+
+    @NonNull
+    public Map<String, Artifact> getClassifiers() {
+      return Collections.unmodifiableMap(this.classifiers);
+    }
+
+    @NonNull
+    public Optional<Artifact> getArtifact() {
+      return Optional.ofNullable(this.artifact);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || this.getClass() != o.getClass()) {
+        return false;
+      }
+      LibraryDownloads that = (LibraryDownloads) o;
+      return Objects.equals(this.classifiers, that.classifiers) &&
+          Objects.equals(this.artifact, that.artifact);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+      return Objects.hash(this.classifiers, this.artifact);
+    }
+  }
+
+  /**
+   * Represents a single downloadable library artifact.
+   */
+  public static final class Artifact {
+
+    private final long size;
+    private final String sha1;
+    private final String path;
+    private final URL url;
+
+    @JsonCreator
+    private Artifact(
+        @JsonProperty(value = "size", required = true) long size,
+        @NonNull @JsonProperty(value = "sha1", required = true) String sha1,
+        @NonNull @JsonProperty(value = "path", required = true) String path,
+        @NonNull @JsonProperty(value = "url", required = true) URL url) {
+      this.size = size;
+      this.sha1 = sha1;
+      this.path = path;
+      this.url = url;
+    }
+
+    public long getSize() {
+      return this.size;
+    }
+
+    @NonNull
+    public String getSha1() {
+      return this.sha1;
+    }
+
+    @NonNull
+    public String getPath() {
+      return this.path;
+    }
+
+    @NonNull
+    public URL getUrl() {
+      return this.url;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || this.getClass() != o.getClass()) {
+        return false;
+      }
+      Artifact artifact = (Artifact) o;
+      return this.size == artifact.size &&
+          Objects.equals(this.sha1, artifact.sha1) &&
+          Objects.equals(this.path, artifact.path) &&
+          Objects.equals(this.url, artifact.url);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+      return Objects.hash(this.size, this.sha1, this.path, this.url);
     }
   }
 }
